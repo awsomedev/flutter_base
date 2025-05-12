732958fe36f0815fa136a3f9510bd55832016353
Sreeyesh <sreeyesh.k@licious.com>
2025-05-12 20:44:52 +0530

    updates

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 
-# Install Flutter
-git clone https://github.com/flutter/flutter.git -b stable
-export PATH="$PATH:`pwd`/flutter/bin"
+# Fail this script if any subcommand fails.
+set -e
 
-# Install Flutter dependencies
+# The default execution directory of this script is the ci_scripts directory.
+cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
+
+# Install Flutter using git.
+git clone https://github.com/flutter/flutter.git --depth 1 -b 3.22.3 $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+# Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
+flutter precache --ios
+
+# Install Flutter dependencies.
 flutter pub get
 
-# Install CocoaPods dependencies
-cd ios
-pod install
-cd ..
+# Install CocoaPods using Homebrew.
+HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
+brew install cocoapods
 
-# Print Flutter version for verification
-flutter --version 
+# Install CocoaPods dependencies.
+cd ios && pod install # run `pod install` in the `ios` directory.
+
+exit 0
